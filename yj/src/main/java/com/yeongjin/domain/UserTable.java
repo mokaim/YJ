46333d84ae679ca1c93534b43036487cc7576ad7
Dayoung <ekdud3460@naver.com>
2020-08-26 10:54:49 +0900

    add detail page

---
+++ yj/src/main/java/com/yeongjin/domain/UserTable.java
@@ -0,0 +1,59 @@
+package com.yeongjin.domain;
+
+public class UserTable {
+	private String ID;
+	private String PW;
+	private String NName; // 닉네임
+	private String Phone;
+	private String Email;
+	
+	public UserTable(String id, String pw, String nname, String phone, String email) {
+		this.ID = id;
+		this.PW = pw;
+		this.NName = nname;
+		this.Phone = phone;
+		this.Email = email;
+	}
+	
+	// Getter Setter
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getPW() {
+		return PW;
+	}
+
+	public void setPW(String pW) {
+		PW = pW;
+	}
+
+	public String getNName() {
+		return NName;
+	}
+
+	public void setNName(String nName) {
+		NName = nName;
+	}
+
+	public String getPhone() {
+		return Phone;
+	}
+
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+}
---
+++ yj/src/main/java/com/yeongjin/persistence/WriteTableDAO.java
@@ -0,0 +1,143 @@
+package com.yeongjin.persistence;
+
+import java.sql.Connection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.yeongjin.domain.WriteTable;
+
+
+public class WriteTableDAO {
+	private WriteTableDAO() {}
+	private static WriteTableDAO instance = null;
+	
+	public static WriteTableDAO getinstance() {
+		if(instance == null)
+			instance = new WriteTableDAO();
+		
+		return instance;
+	}
+	SingletonClass singleton = SingletonClass.getInstance();
+	Connection conn = JDBCUtil.getConnection();
+	
+	public void insertWriteTable(WriteTable writeTable) {
+		singleton.writeInsertCnt = 0;
+		PreparedStatement pstmt = null;
+		String sql = "insert into WriteTable values (?,?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, writeTable.getNName());
+			pstmt.setString(2, writeTable.getHeader());
+			pstmt.setString(3, writeTable.getContent());
+			pstmt.setInt(4, writeTable.getLikes());
+			pstmt.setInt(5, writeTable.getHate());
+			pstmt.setInt(6, writeTable.getVisit());
+			
+			singleton.writeInsertCnt = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("insertWriteTable Error");
+		}
+		finally {
+			try {
+				pstmt.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("pstmtClose Error");
+			}
+		}
+	}
+	
+	public void deleteWriteTable(WriteTable writeTable) {
+		singleton.writeDeleteCnt = 0;
+		PreparedStatement pstmt = null;
+		String sql = "delete from WriteTable where nname = ? and header = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, writeTable.getNName());
+			pstmt.setString(2, writeTable.getHeader());
+			
+			singleton.writeDeleteCnt = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("deleteWriteTable Error");
+		}
+		finally {
+			try {
+				pstmt.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("pstmt Close Error");
+			}
+		}
+	}
+	
+	public void updateLikes(WriteTable writeTable) {
+		singleton.writeUpdateCnt = 0;
+		PreparedStatement pstmt = null;
+		String sql = "update WriteTable set likes +=1 where nname = ? and header = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, writeTable.getNName());
+			pstmt.setString(2, writeTable.getHeader());
+			
+			singleton.writeUpdateCnt = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("updateLikes Error");
+		}
+		finally {
+			try {
+				pstmt.close();
+			}catch(Exception e) {
+				System.out.println("pstmtClose Error");
+			}
+		}
+	}
+	public void updateHates(WriteTable writeTable) {
+		singleton.writeUpdateCnt = 0;
+		PreparedStatement pstmt = null;
+		String sql = "update WriteTable set hates +=1 where nname = ? and header = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, writeTable.getNName());
+			pstmt.setString(2, writeTable.getHeader());
+			
+			singleton.writeUpdateCnt = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("updateHates Error");
+		}
+		finally {
+			try {
+				pstmt.close();
+			}catch(Exception e) {
+				System.out.println("pstmtClose Error");
+			}
+		}
+	}
+	public void updateVisit(WriteTable writeTable) {
+		singleton.writeUpdateCnt = 0;
+		PreparedStatement pstmt = null;
+		String sql = "update WriteTable set visit +=1 where nname = ? and header = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, writeTable.getNName());
+			pstmt.setString(2, writeTable.getHeader());
+			
+			singleton.writeUpdateCnt = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("updateVisit Error");
+		}
+		finally {
+			try {
+				pstmt.close();
+			}catch(Exception e) {
+				System.out.println("pstmtClose Error");
+			}
+		}
+	}
+	
+}
